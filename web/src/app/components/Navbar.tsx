5fe8fb692f37bd728f64604b212d2de9ccffc160
tada-js <justfix010@gmail.com>
2023-06-15 18:25:47 +0900

    Feat : Navbar 유저 프로필

--- web/src/app/components/Navbar.tsx
+++ web/src/app/components/Navbar.tsx
@@ -12,6 +12,7 @@ import {
 import { usePathname } from 'next/navigation';
 import ColorButton from './ui/ColorButton';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Avatar from './Avatar';
 
 const menu = [
   {
@@ -34,6 +35,7 @@ const menu = [
 const Navbar = () => {
   const pathName = usePathname();
   const { data: session } = useSession();
+  const user = session?.user;
 
   return (
     <div className="flex items-center justify-between px-6">
@@ -49,21 +51,30 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
-          {session ? (
-            <ColorButton
-              text="Sign out"
-              onClick={() => {
-                signOut();
-              }}
-            />
-          ) : (
-            <ColorButton
-              text="Sign in"
-              onClick={() => {
-                signIn();
-              }}
-            />
+          {user && (
+            <li>
+              <Link href={`/user/${user.username}`}>
+                <Avatar image={user.image} />
+              </Link>
+            </li>
           )}
+          <li>
+            {session ? (
+              <ColorButton
+                text="Sign out"
+                onClick={() => {
+                  signOut();
+                }}
+              />
+            ) : (
+              <ColorButton
+                text="Sign in"
+                onClick={() => {
+                  signIn();
+                }}
+              />
+            )}
+          </li>
         </ul>
       </nav>
     </div>
