b1bbc7ae6381778425f594d23f17ee51790c0116
tada-js <justfix010@gmail.com>
2023-06-17 03:18:06 +0900

    Feat : posts api 핸들러

---
+++ web/src/app/api/posts/route.ts
@@ -0,0 +1,17 @@
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '../auth/[...nextauth]/route';
+import { getFollowingPostsOf } from '@/service/posts';
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
+  return getFollowingPostsOf(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
