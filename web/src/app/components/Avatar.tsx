df5de7c7146224e92301350a774edb89a3f4f1ed
tada-js <justfix010@gmail.com>
2023-06-16 17:08:49 +0900

    Style : 아바타 컴포넌트

--- web/src/app/components/Avatar.tsx
+++ web/src/app/components/Avatar.tsx
@@ -1,19 +1,37 @@
 interface Props {
   image?: string | null;
+  size?: 'small' | 'normal';
+  highlight?: boolean;
 }
 
-const Avatar = ({ image }: Props) => {
+const Avatar = ({ image, size = 'normal', highlight = false }: Props) => {
   return (
-    <div className="rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 w-9 h-9">
+    <div className={getContainerStyle(size, highlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className="rounded-full p-[0.1rem]"
+        className={`bg-white rounded-full p-[0.1rem] ${getImageSizeStyle(
+          size
+        )}`}
         alt="user profile"
         src={image ?? undefined}
         referrerPolicy="no-referrer"
       />
     </div>
   );
+};
+
+const getContainerStyle = (size: string, highlight: boolean): string => {
+  const baseStyle = 'rounded-full flex justify-center items-center';
+  const highlightStyle = highlight
+    ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
+    : '';
+  const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]';
+
+  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+};
+
+const getImageSizeStyle = (size: string): string => {
+  return size === 'small' ? 'w-[34px] h-[34px]' : 'w-16 h-16 ';
 };
 
 export default Avatar;
