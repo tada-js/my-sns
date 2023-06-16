356f9db251b1ee022404d62d5dac05a9a19c5430
tada-js <justfix010@gmail.com>
2023-06-16 17:07:36 +0900

    Style : 팔로잉바, 사이드바

---
+++ web/src/app/components/SideBar.tsx
@@ -0,0 +1,29 @@
+import { AuthUser } from '@/model/user';
+import Avatar from './Avatar';
+
+interface Props {
+  user: AuthUser;
+}
+
+const SideBar = ({ user: { name, username, image } }: Props) => {
+  return (
+    <>
+      <div className="flex items-center">
+        {image && <Avatar image={image} />}
+        <div className="ml-4">
+          <p className="font-bold">{username}</p>
+          <p className="text-lg leading-4 text-neutral-500">{name}</p>
+        </div>
+      </div>
+      <p className="mt-8 text-sm text-neutral-500">
+        About · Help · Press · API · Jobs · Privacy · Terms · Location ·
+        Language
+      </p>
+      <p className="mt-8 text-sm font-bold text-neutral-500">
+        @Copyright INSTALLGRAM from ME
+      </p>
+    </>
+  );
+};
+
+export default SideBar;
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1 className="text-red-400">My SNS</h1>;
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+import FollowingBar from './components/FollowingBar';
+import PostList from './components/PostList';
+import SideBar from './components/SideBar';
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect('/auth/signin');
+  }
+
+  return (
+    <section className="flex flex-col w-full md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
