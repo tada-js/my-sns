b4549a0765c509c32b87b25a111e6b625f1a2cd3
tada-js <justfix010@gmail.com>
2023-06-24 01:47:25 +0900

    Feat : 팔로우, 언팔로우 SSR 업데이트

--- web/src/app/components/FollowButton.tsx
+++ web/src/app/components/FollowButton.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { HomeUser, ProfileUser } from '@/model/user';
+import { ProfileUser } from '@/model/user';
 import Button from './ui/Button';
 import useMe from '@/hooks/useMe';
+import { useState, useTransition } from 'react';
+import { useRouter } from 'next/navigation';
+import { PulseLoader } from 'react-spinners';
 
 interface Props {
   user: ProfileUser;
@@ -11,6 +14,10 @@ interface Props {
 const FollowButton = ({ user }: Props) => {
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const router = useRouter();
+  const isUpdating = isPending || isFetching;
 
   const showButton = loggedInUser && loggedInUser.username !== username;
   const following =
@@ -19,13 +26,30 @@ const FollowButton = ({ user }: Props) => {
 
   const text = following ? 'Unfollow' : 'Follow';
 
-  const handleFollow = () => {
-    toggleFollow(user.id, !following);
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !following);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
   };
   return (
     <>
       {showButton && (
-        <Button text={text} onClick={handleFollow} red={text === 'Unfollow'} />
+        <div className="relative">
+          {isUpdating && (
+            <div className="absolute inset-0 z-20 flex items-center justify-center">
+              <PulseLoader size={6} color="gray" />
+            </div>
+          )}
+          <Button
+            disabled={isUpdating}
+            text={text}
+            onClick={handleFollow}
+            red={text === 'Unfollow'}
+          />
+        </div>
       )}
     </>
   );
--- web/src/app/components/ui/Button.tsx
+++ web/src/app/components/ui/Button.tsx
@@ -2,15 +2,17 @@ interface Props {
   text: string;
   onClick: () => void;
   red?: boolean;
+  disabled?: boolean;
 }
 
-const Button = ({ text, onClick, red }: Props) => {
+const Button = ({ text, onClick, red, disabled = false }: Props) => {
   return (
     <button
       className={`border-node rounded-md py-2 px-8 text-white font-bold leading-4 ${
         red ? 'bg-red-500 hover:bg-red-600' : 'bg-sky-500 hover:bg-sky-600'
-      }`}
+      } ${disabled && 'opacity-80'}`}
       onClick={() => onClick()}
+      disabled={disabled}
     >
       {text}
     </button>
