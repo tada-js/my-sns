d0682c66dda24dd4db023f4ffc1bf33c0d5468a8
tada-js <justfix010@gmail.com>
2023-06-25 23:14:10 +0900

    Refactor : 사용자 정보 가져오고 사용자 검증하는 유틸 함수 추가 후 변경

--- web/src/app/api/posts/[id]/route.ts
+++ web/src/app/api/posts/[id]/route.ts
@@ -1,19 +1,13 @@
-import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from '../../auth/[...nextauth]/route';
 import { getPost } from '@/service/posts';
+import { withSessionUser } from '@/util/session';
 
 interface Context {
   params: { id: string };
 }
 
 export async function GET(request: NextRequest, context: Context) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!user) {
-    return new Response('Authentication Error', { status: 401 });
-  }
-
-  return getPost(context.params.id).then((data) => NextResponse.json(data));
+  return withSessionUser(async () => {
+    return getPost(context.params.id).then((data) => NextResponse.json(data));
+  });
 }
