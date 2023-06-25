d533c263f29fecb1fac640eb9c702909aa24f9e6
tada-js <justfix010@gmail.com>
2023-06-25 18:59:59 +0900

    Feat : 새 피드 작성 - submit, loading, error

--- web/src/app/components/NewPost.tsx
+++ web/src/app/components/NewPost.tsx
@@ -4,8 +4,10 @@ import { AuthUser } from '@/model/user';
 import PostUserAvatar from './PostUserAvatar';
 import FilesIcon from './ui/icons/FilesIcon';
 import Button from './ui/Button';
-import { ChangeEvent, DragEvent, useState } from 'react';
+import { ChangeEvent, DragEvent, FormEvent, useRef, useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import SyncLoaderSpinner from './ui/SyncLoaderSpinner';
 
 interface Props {
   user: AuthUser;
@@ -14,13 +16,16 @@ interface Props {
 const NewPost = ({ user: { username, image } }: Props) => {
   const [dragging, setDragging] = useState(false);
   const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>();
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const files = e.target?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
   };
 
@@ -42,14 +47,45 @@ const NewPost = ({ user: { username, image } }: Props) => {
     const files = e.dataTransfer?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) {
+      return setError('이미지를 업로드 해주세요.');
+    }
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('text', textRef.current?.value ?? '');
+
+    fetch('/api/posts/', { method: 'POST', body: formData }) //
+      .then((res) => {
+        if (!res.ok) {
+          setError(`${res.status} ${res.statusText}`);
+          return;
+        }
+        router.push('/');
+      }) //
+      .catch((err) => setError(err.toString())) //
+      .finally(() => setLoading(false));
   };
 
   return (
     <section className="flex flex-col items-center w-full max-w-xl mt-6">
+      {loading && (
+        <div className="absolute inset-0 z-20 text-center pt-[30%] bg-sky-500/20">
+          <SyncLoaderSpinner color="red" />
+        </div>
+      )}
+      {error && (
+        <p className="w-full p-4 mb-4 font-bold text-center text-red-600 bg-red-100">
+          {error}
+        </p>
+      )}
       <PostUserAvatar username={username} image={image ?? ''} />
-      <form className="flex flex-col w-full gap-4 mt-2">
+      <form className="flex flex-col w-full gap-4 mt-2" onSubmit={handleSubmit}>
         <input
           className="hidden"
           name="input"
@@ -96,6 +132,7 @@ const NewPost = ({ user: { username, image } }: Props) => {
           required
           rows={10}
           placeholder={'Write a content...'}
+          ref={textRef}
         />
         <Button text="Publish" onClick={() => {}} />
       </form>
